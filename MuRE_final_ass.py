f7f4fbacb452193fe30060f980a27282eb70186f
Nicolas <nicolas.oye@gmx.de>
2021-03-28 23:41:03 +0200

    Implemented the finite difference method for 10 differences

--- MuRE_final_ass.py
+++ MuRE_final_ass.py
@@ -18,33 +18,68 @@ def c_to_p_ideal_gas(c):
 
 # define equation system
 def AES(x):
-    U_large, j_CO_avg, j_H2_avg, C_CO_l, C_H2_l, U_l, eps_large_avg, R_CO, R_H2, kLa_CO_avg, kLa_H2_avg, U_large_avg, p_CO_l, p_H2_l, eps_slurry = x
+    U_lb_1, j_CO_1, j_H2_1, U_lb_2, j_CO_2, j_H2_2, U_lb_3, j_CO_3, j_H2_3, U_lb_4, j_CO_4, j_H2_4, U_lb_5, j_CO_5, j_H2_5, \
+    U_lb_6, j_CO_6, j_H2_6, U_lb_7, j_CO_7, j_H2_7, U_lb_8, j_CO_8, j_H2_8, U_lb_9, j_CO_9, j_H2_9, U_lb_10, j_CO_10, j_H2_10, \
+    C_CO_l, C_H2_l, U_l, R_CO, R_H2, p_CO_l, p_H2_l, eps_slurry = x
     return [
         # balances
-        (U_large_previous_stage - U_large) * (C_CO_large + C_H2_large) - H_stage * (j_CO_avg + j_H2_avg),
-        H_stage * j_CO_avg + C_CO_l_previous_stage * U_l_previous_stage - C_CO_l * U_l - H_stage * eps_slurry * R_CO,
-        H_stage * j_H2_avg + C_H2_l_previous_stage * U_l_previous_stage - C_H2_l * U_l - H_stage * eps_slurry * R_H2,
+        (U_lb_1 - U_lb_previous_stage) / h + (j_CO_1 + j_H2_1) / (C_CO_lb + C_H2_lb),
+        j_CO_1 - (0.3 * U_lb_1**0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_1 - (0.3 * U_lb_1**0.58 * np.sqrt(D_H2/ D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_2 - U_lb_1) / h + (j_CO_2 + j_H2_2) / (C_CO_lb + C_H2_lb),
+        j_CO_2 - (0.3 * U_lb_2**0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_2 - (0.3 * U_lb_2**0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_3 - U_lb_2) / h + (j_CO_3 + j_H2_3) / (C_CO_lb + C_H2_lb),
+        j_CO_3 - (0.3 * U_lb_3 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_3 - (0.3 * U_lb_3 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_4 - U_lb_3) / h + (j_CO_4 + j_H2_4) / (C_CO_lb + C_H2_lb),
+        j_CO_4 - (0.3 * U_lb_4 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_4 - (0.3 * U_lb_4 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_5 - U_lb_4) / h + (j_CO_5 + j_H2_5) / (C_CO_lb + C_H2_lb),
+        j_CO_5 - (0.3 * U_lb_5 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_5 - (0.3 * U_lb_5 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_6 - U_lb_5) / h + (j_CO_6 + j_H2_6) / (C_CO_lb + C_H2_lb),
+        j_CO_6 - (0.3 * U_lb_6 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_6 - (0.3 * U_lb_6 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_7 - U_lb_6) / h + (j_CO_7 + j_H2_7) / (C_CO_lb + C_H2_lb),
+        j_CO_7 - (0.3 * U_lb_7 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_7 - (0.3 * U_lb_7 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_8 - U_lb_7) / h + (j_CO_8 + j_H2_8) / (C_CO_lb + C_H2_lb),
+        j_CO_8 - (0.3 * U_lb_8 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_8 - (0.3 * U_lb_8 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_9 - U_lb_8) / h + (j_CO_9 + j_H2_9) / (C_CO_lb + C_H2_lb),
+        j_CO_9 - (0.3 * U_lb_9 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_9 - (0.3 * U_lb_9 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        (U_lb_10 - U_lb_9) / h + (j_CO_10 + j_H2_10) / (C_CO_lb + C_H2_lb),
+        j_CO_10 - (0.3 * U_lb_10 ** 0.58 * np.sqrt(D_CO / D_ref) * (C_CO_lb / H_CO - C_CO_l)),
+        j_H2_10 - (0.3 * U_lb_10 ** 0.58 * np.sqrt(D_H2 / D_ref) * (C_H2_lb / H_H2 - C_H2_l)),
+
+        h * (j_CO_1 + j_CO_2 + j_CO_3 + j_CO_4 + j_CO_5 + j_CO_6 + j_CO_7 + j_CO_8 + j_CO_9 + j_CO_10) + C_CO_l_previous_stage * U_l_previous_stage - C_CO_l * U_l - H_stage * eps_slurry * R_CO,
+        h * (j_H2_1 + j_H2_2 + j_H2_3 + j_H2_4 + j_CO_5 + j_H2_6 + j_H2_7 + j_H2_8 + j_H2_9 + j_H2_10) + C_H2_l_previous_stage * U_l_previous_stage - C_H2_l * U_l - H_stage * eps_slurry * R_H2,
         # constituent equations
-        j_CO_avg - (kLa_CO_avg * (C_CO_large / H_CO - C_CO_l)),
-        j_H2_avg - (kLa_H2_avg * (C_H2_large / H_H2 - C_H2_l)),
-        U_large_avg - (0.5 * (U_large + U_large_previous_stage)),
-        eps_large_avg - (0.3 * abs(U_large_avg)**0.58),
-        kLa_CO_avg - (eps_large_avg * 0.5 * np.sqrt(D_CO / D_ref)),
-        kLa_H2_avg - (eps_large_avg * 0.5 * np.sqrt(D_H2 / D_ref)),
         U_l - (U_l_previous_stage + (H_stage * eps_slurry * A_reactor * R_CO * M_CO + H_stage * eps_slurry * A_reactor * R_H2 * M_H2) / rho_slurry / A_reactor),
         R_CO - (F * a * p_H2_l * p_CO_l / (1 + b * p_CO_l)**2 * eps_cat * rho_skeleton_cat),
         R_H2 - (2 * R_CO),
         p_CO_l - (H_CO * C_CO_l * R * T * 10**-5),
         p_H2_l - (H_H2 * C_H2_l * R * T * 10**-5),
-        eps_slurry - (1 - eps_large_avg),
+        eps_slurry - (1 - 0.3 * U_lb_5**0.58),
             ]
+
 
 #####################################################################################################
 # parameter and variable definition
-
 # design variables
-number_of_stages = 1    # number of stages
-height_reactor = 5      # reactor diameter in m
+number_of_stages = 5    # number of stages
+height_reactor = 5     # reactor diameter in m
 diam_reactor = 7        # reactor diameter in m
 U_in = 0.3              # inlet superficial gas velocity in m/s
 P = 30 * 10**5          # pressure in Pa
@@ -52,8 +87,9 @@ T = 240 + 273.15        # temperature in K
 dp = 50 * 10**-6        # catalyst particle size in m
 eps_cat = 0.35          # catalyst concentration in vol_cat/vol_slurry
 feed_ratio = 2          # ratio of C_H2/C_CO
+h = height_reactor / number_of_stages / 10  # finite difference step size
 # inlet conditions
-U_large_previous_stage = U_in
+U_lb_previous_stage = U_in
 C_CO_l_previous_stage = 0  # 0 for fresh slurry
 C_H2_l_previous_stage = 0  # 0 for fresh slurry
 U_l_previous_stage = 0  # 0 for semi-batch
@@ -86,15 +122,15 @@ F = 3                       # catalyst activity multiplication factor (assignmen
 
 # calculate dependent parameters
 # bubble concentrations
-C_CO_large = p_to_c_ideal_gas(P / (1 + feed_ratio))
-C_H2_large = p_to_c_ideal_gas(P / (1 + 1 / feed_ratio))
+C_CO_lb = p_to_c_ideal_gas(P / (1 + feed_ratio))
+C_H2_lb = p_to_c_ideal_gas(P / (1 + 1 / feed_ratio))
 # kinetic parameters
 a = a0 * np.exp(Ea/R * (1/493.15 - 1/T))        # langmuir hinshelwood parameter 1 mol/s/kgcat/bar2
 b = b0 * np.exp(deltabH/R * (1/493.15 - 1/T))   # langmuir hinshelwood parameter 2 in 1/bar
 # hydrodynamic parameters
 A_reactor = np.pi * diam_reactor ** 2 / 4  # reactor cross section area in m2
 H_stage = height_reactor/number_of_stages
-U_large = 1  # superficial gas velocity for large bubbles in m/s
+U_lb = 1  # superficial gas velocity for lb bubbles in m/s
 U_small = 1  # superficial gas velocity for small bubbles in m/s
 # slurry parameters
 rho_slurry = rho_solvent * (1 - rho_solvent/rho_skeleton_cat * eps_cat) + rho_cat * eps_cat # slurry density in kg/m3 (maretto2009, 23)
@@ -106,47 +142,87 @@ visco_slurry = visco_solvent * (1 + 4.5 * eps_cat)                              
 
 # initial guess definition
 initial_guess = np.array([
-    U_in,  # U_large
+    U_in,  # U_lb
     1,  # j_CO_avg
     1,  # j_H2_avg
-    C_CO_large,  # C_CO_l
-    C_H2_large,  # C_H2_l
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    U_in,  # U_lb
+    1,  # j_CO_avg
+    1,  # j_H2_avg
+    C_CO_lb / H_CO,  # C_CO_l
+    C_H2_lb / H_H2,  # C_H2_l
     1,  # U_l
-    0.5,  # eps_large_avg
     1,  # R_CO
     1,  # R_H2
-    1,  # k_La_CO_avg
-    1,  # k_La_H2_avg
-    U_in,  # U_large_avg
-    c_to_p_ideal_gas(C_CO_large) * 10**-5,  # p_CO_l
-    c_to_p_ideal_gas(C_H2_large) * 10**-5,  # p_H2_l
+    c_to_p_ideal_gas(C_CO_lb / H_CO) * 10**-5,  # p_CO_l
+    c_to_p_ideal_gas(C_H2_lb / H_H2) * 10**-5,  # p_H2_l
     0.5   # eps_slurry
 ])
 
+print(initial_guess)
 stage_solutions = []
 for stage_counter in range(number_of_stages):
-    sol, output, ier, mesg = optimize.fsolve(AES, initial_guess, full_output=True)
+    # sol, output, ier, mesg = optimize.fsolve(AES, initial_guess, full_output=True)
     # print(ier)
-    print(mesg)
-    # root = optimize.root(AES, initial_guess)
-    # sol = root.x
+    # print(mesg)
+    root = optimize.root(AES, initial_guess)
+    sol = root.x
     stage_solutions.append(sol)  # save solutions of this stage
     initial_guess = sol  # the initial guess for the next stage
 
-    print('residuals: ',AES(sol))
+    # print('residuals: ',AES(sol))
     print('sum of residuals = ', sum(AES(sol)))
     print()
+    print('U_lb_out =\t', round(sol[27], 3))
+    print('C_CO_l =  \t', round(sol[30], 3))
+    print('C_H2_l =  \t', round(sol[31], 3))
+    print('U_l =     \t', round(sol[32], 3))
 
     # define boundary inlet conditions for the next stage
-    U_large_previous_stage = sol[0]
-    C_CO_l_previous_stage = sol[3]
-    C_H2_l_previous_stage = sol[4]
-    U_l_previous_stage = sol[5]
+    U_lb_previous_stage = sol[27]
+    C_CO_l_previous_stage = sol[30]
+    C_H2_l_previous_stage = sol[31]
+    U_l_previous_stage = sol[32]
 
-sol_labels = ['U_large', 'j_CO_avg', 'j_H2_avg', 'C_CO_l', 'C_H2_l', 'U_l', 'eps_large_avg', 'R_CO', 'R_H2', 'kLa_CO_avg', 'kLa_H2_avg', 'U_large_avg', 'p_CO_l', 'p_H2_l', 'eps_slurry']
-for i in range(len(sol_labels)):
-    print('%s =\t %s' % (sol_labels[i], round(sol[i],5)))
+# sol_labels = ['U_lb1', 'j_CO1', 'j_H21', 'U_lb2', 'j_CO2', 'j_H22', 'C_CO_l', 'C_H2_l', 'U_l', 'R_CO', 'R_H2', 'p_CO_l', 'p_H2_l', 'eps_slurry']
+# for i in range(len(sol_labels)):
+#     print('%s =\t %s' % (sol_labels[i], round(sol[i],5)))
 
+print('The final conversion is %s percent' % (round(1 - U_lb_previous_stage/U_in, 2)*100))
+
+plt.figure()
+plt.plot(0, U_in, 'o', color='blue')
+for i in range(5):
+    for j in range(10):
+        plt.plot(i + h * (j+1),stage_solutions[i][j*3], 'o', color='blue')
+plt.xlabel('Height in m')
+plt.ylabel('Superficial gas velocity in m/s')
+plt.show()
+
+print(stage_solutions)
 exit()
 ################################################################
 # sensitivity analysis
